f769053f7956f236b70fbbd7593b215f9d7aca52
Michael Kralka <mkralka@proofpoint.com>
2012-12-07 16:27:34 -0800

    Add unit test for EventTapWriter.write()
    This unit test makes sure events passed into write() get written to the
    correct URL.

--- src/test/java/com/proofpoint/event/collector/TestEventTapWriter.java
+++ src/test/java/com/proofpoint/event/collector/TestEventTapWriter.java
@@ -1,19 +1,38 @@
 package com.proofpoint.event.collector;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.proofpoint.discovery.client.ServiceDescriptor;
 import com.proofpoint.discovery.client.ServiceSelector;
+import com.proofpoint.discovery.client.ServiceState;
 import com.proofpoint.discovery.client.testing.StaticServiceSelector;
+import com.proofpoint.http.client.BodyGenerator;
 import com.proofpoint.http.client.HttpClient;
+import com.proofpoint.http.client.Request;
+import com.proofpoint.http.client.ResponseHandler;
 import com.proofpoint.json.JsonCodec;
+import org.joda.time.DateTime;
 import org.logicalshift.concurrent.SerialScheduledExecutorService;
+import org.mockito.ArgumentCaptor;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 
+import static java.util.UUID.randomUUID;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class TestEventTapWriter
 {
@@ -58,5 +77,109 @@ public class TestEventTapWriter
     public void testConstructorNullConfig()
     {
         new EventTapWriter(serviceSelector, httpClient, OBJECT_CODEC, executorService, null);
+    }
+
+    @Test
+    public void testWrite()
+            throws Exception
+    {
+        int maxEventsPerType = 10;
+        int numEventTypes = 10;
+
+        for (int i = 1; i <= maxEventsPerType; ++i) {
+            reset(httpClient);
+            testWrite(numEventTypes, maxEventsPerType, i);
+        }
+    }
+
+    private void testWrite(int numEventTypes, int eventCountPerType, int batchSize)
+            throws Exception
+    {
+        ArgumentCaptor<Request> requestArgumentCaptor = ArgumentCaptor.forClass(Request.class);
+        Map<String, List<Event>> events = new HashMap<String, List<Event>>();
+
+        ImmutableList.Builder<ServiceDescriptor> serviceDescriptorBuilder = ImmutableList.<ServiceDescriptor>builder();
+        for (int i_type = 0; i_type < numEventTypes; ++i_type) {
+            String type = String.format("Type%d", i_type);
+            List<Event> typeEvents = new ArrayList<Event>();
+            for (int i_event = 0; i_event < eventCountPerType; ++i_event) {
+                typeEvents.add(createEvent(type));
+            }
+            events.put(type, typeEvents);
+
+            String nodeId = randomUUID().toString();
+            ServiceDescriptor descriptor = new ServiceDescriptor(
+                    randomUUID(),
+                    nodeId,
+                    "EventTap",
+                    "global",
+                    "/" + nodeId,
+                    ServiceState.RUNNING,
+                    ImmutableMap.<String, String>of("eventType", type, "tapId", "1", "http", String.format("http://%s.event.tap", type))
+            );
+            serviceDescriptorBuilder.add(descriptor);
+        }
+
+        EventTapWriter writer = new EventTapWriter(
+                new StaticServiceSelector(serviceDescriptorBuilder.build()),
+                httpClient,
+                OBJECT_CODEC,
+                executorService,
+                new EventTapConfig().setMaxBatchSize(batchSize));
+        writer.refreshFlows();
+
+        for (List<Event> eventsOfType : events.values()) {
+            for (Event event : eventsOfType) {
+                writer.write(event);
+            }
+        }
+
+        // UGH!!! BatchProcessor creates it's own thread to process stuff.
+        // For now, just sleep to give the thread enough time to handle
+        // the events.
+        Thread.sleep(1000);
+
+        verify(httpClient, atLeastOnce()).execute(requestArgumentCaptor.capture(), any(ResponseHandler.class));
+
+        for (Request request : requestArgumentCaptor.getAllValues()) {
+            List<Event> receivedEvents = getBody(request);
+            String type = request.getUri().getHost().split("\\.")[0];
+            List<Event> remainingEvents = events.get(type);
+            String context = String.format("size=%d type=%s countPerType=%d batchSize=%d remaining=%d",
+                    receivedEvents.size(), type, eventCountPerType, batchSize, remainingEvents.size());
+
+            int size = receivedEvents.size();
+            assertTrue(size <= batchSize, context);
+
+            for (int i = 0; i < size; ++i) {
+                checkEquals(receivedEvents.get(i), remainingEvents.get(i));
+            }
+
+            for (int i = 0; i < size; ++i) {
+                remainingEvents.remove(0);
+            }
+
+        }
+    }
+
+    private void checkEquals(Event actual, Event expected)
+    {
+        assertEquals(actual.getUuid(), actual.getUuid());
+        assertEquals(actual.getType(), expected.getType());
+        assertEquals(actual.getHost(), expected.getHost());
+    }
+
+    private Event createEvent(String type)
+    {
+        return new Event(type, randomUUID().toString(), "host", DateTime.now(), ImmutableMap.<String, Object>of());
+    }
+
+    private List<Event> getBody(Request request)
+            throws Exception
+    {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        BodyGenerator bodyGenerator = request.getBodyGenerator();
+        bodyGenerator.write(byteArrayOutputStream);
+        return OBJECT_CODEC.fromJson(byteArrayOutputStream.toString());
     }
 }
